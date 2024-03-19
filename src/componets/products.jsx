a43b516c59cb8cec2d324b3daebc1251067ca334
dennisbc24 <dennis.bien.vida@gmail.com>
2024-03-19 16:14:38 -0500

    414

--- src/componets/products.jsx
+++ src/componets/products.jsx
@@ -1,16 +1,27 @@
 import "./data.json";
-
+import "./products.css"
 export const Products = ({list}) => {
     return (
-        <main>
+        <main className="list_products">
             <ul>
-                {
-                    list.map(item => (
-                        <li key={item.id_product}>
-                            <p>{item.name}</p>
-                            <p>{item.cost}</p>
-                            <p>{item.lowest_price}</p>
-                        </li>
+                {list.map(item => (
+                    <li key={item._id} className="card">
+                        <div className="card_info">
+                            <h3>{item.name}</h3>
+                            <p>S/.{item.price}</p>
+                            <div>
+                                <img src={item.imageUrl} alt={item.name} className="card_image"/>
+                            </div>
+                            <div className="characteristics">
+                                {item.caracteristicas.map(carac =>{
+                                    return(<p key={crypto.randomUUID()}>
+                                        {carac}
+                                    </p>)
+                                })}
+                            </div>
+                        </div>
+                            
+                    </li>
                     ))
                 }
             </ul>
